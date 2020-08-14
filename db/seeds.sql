e1c4c0d121da9b8c749c5b3c2c60292259fcb920
JoeL <jlawton1203@gmail.com>
2020-08-14 09:59:00 -0500

    designed database schema, created schema.sql and seeds.sql for testing

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ("sales"), ("administration"), ("development"), ("information technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("sales person", 65432, 1), ("sales manager", 76543, 1), ("web developer", 65432, 3), ("system administrator", 65432, 4), ("administrative assistant", 54321, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Manager", "Man", 3, null), ("Joe", "Lawton", 3, 1), ("Sales", "Manager", 2, null), ("Sales", "Guy", 1, 3), ("Mr", "UNIX", 4, null), ("Girl", "Friday", 5, null);
